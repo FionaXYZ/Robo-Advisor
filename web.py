a9d5c028bb376cc7f230e02ca6d3581ea43db1bd
赵栩滢 <zhaoxuying@zhaoxuyingdeMacBook-Air.local>
2020-12-02 21:28:36 +0000

    web scraping of funds including their names,ISIN and 3 year annualised

--- web.py
+++ web.py
@@ -4,16 +4,28 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
-        'http://quotes.toscrape.com/tag/humor/',
+        'https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F00000X8RW',
+        'https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F00000NGEL',
+        'https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F00000MDAJ',
+        'https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F00000Y0Q0',
+        'https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F00000LWRT',
     ]
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
             yield {
-                'author': quote.xpath('span/small/text()').get(),
-                'text': quote.css('span.text::text').get(),
+                'Name':response.xpath('//div[@id="snapshotTitleDiv"]//div[@class="snapshotTitleBox"]/h1/text()').get(),
+                'ISIN':response.xpath('//div[@id="overviewQuickstatsDiv"]//td[contains(.,"ISIN")]/../td[@class="line text"]/text()').get(),
+                '3 year annualised': response.xpath('//div[@id="overviewTrailingReturnsDiv"]//td[contains(.,"3 Years Annualised")]/../td[@class="value number"]/text()').get(),             
             }
 
-        next_page = response.css('li.next a::attr("href")').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+
+
+
+
+
+
+
+
+        # next_page = response.css('li.next a::attr("href")').get()
+        # if next_page is not None:
+        #     yield response.follow(next_page, self.parse)
