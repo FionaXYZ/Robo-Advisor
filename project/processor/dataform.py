cd32e078b174149d250fb2c5af94093c0f54082f
赵栩滢 <zhaoxuying@zhaoxuyingdeMacBook-Air.local>
2021-01-09 15:21:00 +0000

    imputation of the data and fill all information in one file

--- project/processor/dataform.py
+++ project/processor/dataform.py
@@ -26,29 +26,61 @@ for isin in prices:
     sorted(prices[isin], key = lambda t: t[0])
     # sorted() function best case complexity is O(n)
 
+modified_prices={isin["ISIN"]:[] for isin in datas_mornid}
+#imputation
+def impute(modified_prices,prices):
+    for isin in prices:
+        for pair in range(len(prices[isin])-1):
+            diff=(prices[isin][pair][0]-prices[isin][pair+1][0]).days
+            modified_prices[isin].append(float(prices[isin][pair][1]))
+            if diff>1:
+                price_now=float(prices[isin][pair][1])
+                price_next=float(prices[isin][pair+1][1])
+                ratio=(price_next/price_now)**(1/diff)
+                next=price_now*ratio
+                while diff>1:
+                    #rounding only happens at output
+                    modified_prices[isin].append(round(next,2))
+                    next*=ratio
+                    diff-=1
+        modified_prices[isin].append(float(prices[isin][-1][1]))
+
+
+impute(modified_prices,prices)
+
+#find common start and end date
+start=max(prices[isin][-1][0] for isin in prices)
+end=min(prices[isin][0][0] for isin in prices)
+
+for isin in prices:
+    diff_end=(prices[isin][0][0]-end).days
+    diff_start=(start-prices[isin][-1][0]).days
+    modified_prices[isin]=modified_prices[isin][diff_end:len(modified_prices[isin])-diff_start]
 
 
 
 
 
+# processing the data looks like [{'ISIN': '', '3_year_annalised': '', '3_year_sd': ''},{"prices":[]}]
+process=[]
+for data_m in datas_mornid:
+    process.append({"ISIN":data_m["ISIN"]})
 
-#processing the data looks like [{'ISIN': '', '3_year_annalised': '', '3_year_sd': ''},{....}]
-# process=[]
-# for data_m in datas_mornid:
-#     process.append({"ISIN":data_m["ISIN"]})
+for data in process:
+    for data_r in datas_rate:
+        if data_r["ISIN"]==data["ISIN"]:
+            if "3_year_annualised" in data_r:
+                data["3_year_annalised"]=data_r["3_year_annualised"]
+            else:
+                data["3_year_sd"]=data_r["3_year_sd"]
+    for isin in modified_prices:
+        if data["ISIN"]==isin:
+            data["prices"]=modified_prices[isin]
+            break
+output={"data":process,"meta":{"start":datetime.strftime(start,"%Y/%m/%d"),"end":datetime.strftime(end,"%Y/%m/%d")}}
 
-# for data in process:
-#     for data_r in datas_rate:
-#         if data_r["ISIN"]==data["ISIN"]:
-#             if "3_year_annualised" in data_r:
-#                 data["3_year_annalised"]=data_r["3_year_annualised"]
-#             else:
-#                 data["3_year_sd"]=data_r["3_year_sd"]
-
-# with open('../data_out/model_input.jl', 'w') as outfile:
-#     for processed in process:
-#         json.dump(processed, outfile)
-#         outfile.write('\n')
+with open('data_out/model_input.json', 'w') as outfile:
+    json.dump(output, outfile)
 
 
 
