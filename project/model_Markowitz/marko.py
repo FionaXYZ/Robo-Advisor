cf4bfa332df48a70e590aab8504790e1dec78a9e
赵栩滢 <zhaoxuying@zhaoxuyingdeMacBook-Air.local>
2021-01-29 16:24:03 +0000

    initial Markowitz model

--- project/model_Markowitz/marko.py
+++ project/model_Markowitz/marko.py
@@ -1,6 +1,7 @@
 import numpy as np
 import json
 import math
+from scipy.optimize import minimize
 
 with open('data_out/model_input.json') as json_file:
     data = json.load(json_file)
@@ -16,7 +17,7 @@ for datas in data["data"]:
     std.append(datas["3_year_sd"])
     returns.append(datas["3_year_annalised"])
 
-print(returns)
+
 rates=np.array(all)
 covMatrix = np.cov(rates,bias=True)
 
@@ -24,4 +25,33 @@ mod=std/(np.diag(covMatrix)**0.5)
 
 model_input=mod*covMatrix*np.array(mod)[np.newaxis].T
 #np.diag(mod)*covMatrix*np.diag(mod) == np.diag(np.array(std)**2)
+
+
+n_assets=len(returns)
+returns = np.array(returns)
+
+
+def make_marko(C):
+    def marko(w):
+        return w.dot(C.dot(w))
+    return  marko
+
+def make_eq(target,R):
+    return lambda w: target-w.dot(R)
+
+def summ(w):
+    return 1.0 - np.sum(w)
+
+# init=np.random.rand(n_assets)
+# w0 = init/sum(init)
+w0 = [1/n_assets]*n_assets
+w0=np.array(w0)
+
+bounds = ((0.0, 1.0),) * len(returns)
+
+weights = minimize(make_marko(model_input), w0, method='SLSQP', constraints=({'type': 'eq', 'fun': make_eq(10,returns)},{'type': 'eq', 'fun': summ}),bounds=bounds)
+# print(10)
+# print(weights.x)
+
+
 
