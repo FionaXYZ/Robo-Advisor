correct sampling rates & Get minimum variance set return(median of all the frequencies)

project/model_Markowitz/marko.py
@@ -23,12 +23,37 @@ for datas in data["data"]:
     returns.append(datas["3_year_annalised"])
 
 n_assets=len(returns)
-returns = np.array(returns)
-print(f"maximum return you can choose is {max(returns)}")
-# minimum=min(returns)
-# print(minimum)
-target_returns=[10,10.25,10.5,10.75,11,11.25,11.5,11.75,12,12.25,12.5,12.75,13,13.25,13.5,13.75,14]
+returns=np.array(returns)
+maxi=max(returns)
+print(f"maximum return you can choose is {maxi}")
 
+# find minimum return(at minimum variance set)
+mini=min(returns)
+ret={key:[] for key in category}
+minimum=[]
+for rate in ret:
+    for datas in data["data"]:
+        ret[rate].append(datas[rate])
+    ret[rate]=np.array(ret[rate])
+    covMatrix=np.cov(ret[rate],bias=True)
+    mod=std/(np.diag(covMatrix)**0.5)
+    model_input=mod*covMatrix*np.array(mod)[np.newaxis].T
+    w=cp.Variable(n_assets)
+    marko=cp.Problem(cp.Minimize((1/2)*cp.quad_form(w, model_input)),[w.T*returns>=mini,cp.sum(w) == 1,w>=0])
+    marko.solve()
+    minimum.append(np.dot(w.value,returns))
+print(minimum)
+mini=np.median(np.array(minimum))
+print(f"The minimum return available {mini}")
+# find the returns on efficient frontier
+target_returns=[]
+while mini<maxi:
+    target_returns.append(mini)
+    mini+=0.25
+target_returns.append(maxi)
+
+target_returns=[10,10.25,10.5,10.75,11,11.25,11.5,11.75,12,12.25,12.5,12.75,13,13.25,13.5,13.75,14]
+# Get and Draw efficient frontier for different sampling rate
 for rate in rates:
     for datas in data["data"]:
         rates[rate].append(datas[rate])
@@ -37,19 +62,11 @@ for rate in rates:
     mod=std/(np.diag(covMatrix)**0.5)
     model_input=mod*covMatrix*np.array(mod)[np.newaxis].T
     for target in target_returns:
-        w = cp.Variable(n_assets)
-        marko = cp.Problem(cp.Minimize((1/2)*cp.quad_form(w, model_input)), [w.T * returns >= target, cp.sum(w) == 1, w>=0])
+        w=cp.Variable(n_assets)
+        marko=cp.Problem(cp.Minimize((1/2)*cp.quad_form(w, model_input)),[w.T*returns>=target,cp.sum(w) == 1,w>=0])
         marko.solve()
         res[rate]["weights"].append(w.value)
         res[rate]["variance"].append(marko.value)
-
-# curve={}
-# begin=res[category[0]]["variance"]
-# for rate in res:
-#     if res[rate]["variance"]>begin:
-#         curve[rate]=res[rate]
-# print(curve)
-
 
 for rate in res:
     WCW=res[rate]["variance"]
@@ -63,30 +80,31 @@ plt.show()
 
 weights={key:{"weights":[]} for key in target_returns}
 number=0
-for returns in target_returns:
+for ret in target_returns:
     for rate in res: 
-        weights[returns]["weights"].append(res[rate]["weights"][number])
+        weights[ret]["weights"].append(res[rate]["weights"][number])
     number=number+1
 
 weights_avg={key:{"mean":[],"difference":[]} for key in target_returns}  
 
-for returns in target_returns:
+for ret in target_returns:
     diff=[]
-    mean=np.mean(weights[returns]["weights"],axis=0)
-    weights_avg[returns]["mean"].append(mean)
-    max=np.amax(weights[returns]["weights"],axis=0)
-    min=np.amin(weights[returns]["weights"],axis=0)
+    mean=np.mean(weights[ret]["weights"],axis=0)
+    weights_avg[ret]["mean"].append(mean)
+    max=np.amax(weights[ret]["weights"],axis=0)
+    min=np.amin(weights[ret]["weights"],axis=0)
     diff.append(max-mean)
     diff.append(mean-min)
-    weights_avg[returns]["difference"].append(np.amax(diff,axis=0))
+    weights_avg[ret]["difference"].append(np.amax(diff,axis=0))
 
+print(weights_avg)
 
 asset=0
 allocation=[]
 while asset<n_assets:
     weight=[]
-    for returns in target_returns: 
-        weight.append(weights_avg[returns]["mean"][0][asset])
+    for ret in target_returns: 
+        weight.append(weights_avg[ret]["mean"][0][asset])
     asset=asset+1
     allocation.append(weight)
 