0b54d5ff7be6f6b81d0b11fdc67687d6cd6e434c
赵栩滢 <zhaoxuying@zhaoxuyingdeMacBook-Air.local>
2021-05-23 01:23:46 +0100

    interface

--- project/ui/src/App.js
+++ project/ui/src/App.js
@@ -1,20 +1,29 @@
 import React,{useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ISINOption from './ISINOption'
+import Button from '@material-ui/core/Button';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
-      width: '25ch',
+      // width: '25ch',
     },
   },
 }));
 
+
+
 export default function AssetSelection() {
   const classes=useStyles();
   const default_isin=['LU1053186349','IE00B3VNP587','LU0712206050','LU1041109759','IE00B530JS22'];
-  const [isins,setIsins]=useState(default_isin)
+
+  function makeDefualtIsinObj(isin) {
+    return{"isin":isin,"constraint":"None","constraint_op1":null}
+  }
+
+  const [isins,setIsins]=useState(default_isin.map(makeDefualtIsinObj))
 
   function factoryUpdateArray(array,idx) {
     return (newVal=>{
@@ -24,16 +33,34 @@ export default function AssetSelection() {
     })
   }
 
+  function NewIsin(array){
+      let newArray=[...array]
+      newArray.push(makeDefualtIsinObj(""))
+      setIsins(newArray);
+  }
+
+  function DeleteIsin(array,idx){
+    let newArray=[...array]
+    if (idx>-1) {
+      newArray.splice(idx, 1);
+      setIsins(newArray);
+    }
+  }
+
+
   console.log(isins)
   
   return (
+    <>
     <form className={classes.root} noValidate autoComplete="off">
     {isins.map((isin,i)=>
-        (<ISINOption isin={isin} setIsin={factoryUpdateArray(isins,i)}/>)
-      )}
+        (<ISINOption isinObj={isin} setIsinObj={factoryUpdateArray(isins,i)} deleteIsin={()=>{DeleteIsin(isins,i)}} key={i}/> 
+    ))}
+
+    <Button onClick={()=>{NewIsin(isins)}} color="primary" variant="contained">Add New ISIN</Button>
     </form>
+    </>
   )
 }
-
 
 
--- project/ui/src/ISINOption.js
+++ project/ui/src/ISINOption.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-  
-export default function ISINOption({isin,setIsin}) {
-    console.log(isin)
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const constraints=[
+  {
+    value:"None",
+    label:'None',
+  },
+  {
+    value:'>=',
+    label: '≥',
+  },
+  {
+    value:'<=',
+    label:'≤',
+  },
+];
+
+
+export default function ISINOption({isinObj,setIsinObj,deleteIsin}) {
+    console.log(isinObj)
 
     return (
       <>
-        <TextField label="ISIN" id="standard-basic" defaultValue={isin} onChange={e=>setIsin(e.target.value)}/>
-        <TextField id="filled-basic" label="Filled" variant="filled"/>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined"/> <br/>
+        <TextField label="ISIN" id="standard-basic" defaultValue={isinObj.isin} onChange={e=>setIsinObj({...isinObj,isin:e.target.value})} variant="outlined"/>
+        <TextField id="select-constraints" select label="Constraints"  defaultValue={isinObj.constraint} onChange={e=>{setIsinObj({...isinObj,constraint:e.target.value})}} 
+          SelectProps={{native: true,}}
+          helperText="Choose to add constraint"
+          variant="outlined" >
+          {constraints.map((option) => (
+            <option key={option.value} >
+              {option.label}
+            </option>
+          ))}
+        </TextField>
+        {isinObj.constraint!=="None" && <TextField id="constraints-value" label="0<value<1" defaultValue={isinObj.constraint_op1} onChange={e=>setIsinObj({...isinObj,constraint_op1:e.target.value})} variant="outlined" />}
+         <Button startIcon={<DeleteIcon />} onClick={()=>{deleteIsin()}} color="Gray" variant="contained"></Button>
+         <br/>
       </>
     );
 }
