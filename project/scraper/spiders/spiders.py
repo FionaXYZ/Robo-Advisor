1f9d1ed543b02ee6360bb2194847fdd8f074733a
赵栩滢 <zhaoxuying@zhaoxuyingdeMacBook-Air.local>
2021-05-27 21:47:26 +0100

    update scraper with today's date(in the comment for future use)

--- project/scraper/spiders/spiders.py
+++ project/scraper/spiders/spiders.py
@@ -1,6 +1,8 @@
 import scrapy
 from scrapy.selector import Selector
 import json
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 contents = open('data/ftid_no.jl', "r").read() 
 datas = [json.loads(str(item)) for item in contents.strip().split('\n')]
@@ -9,7 +11,13 @@ datas = [json.loads(str(item)) for item in contents.strip().split('\n')]
 class SpiderHistoricalPrice(scrapy.Spider):
     name = 'historical'
     start_urls=[]
-    link_format='https://markets.ft.com/data/equities/ajax/get-historical-prices?startDate=2018%2F01%2F08&endDate=2021%2F01%2F08&symbol='
+    # start_date=datetime.today()-relativedelta(years=3)
+    # end_date=datetime.today()
+    # start_date=datetime.strftime(start_date,"%Y%%2F%m%%2F%d")
+    # end_date=datetime.strftime(end_date,"%Y%%2F%m%%2F%d")
+    start_date="2018%2F01%2F08"
+    end_date="2021%2F01%2F08"
+    link_format=f'https://markets.ft.com/data/equities/ajax/get-historical-prices?startDate={start_date}&endDate={end_date}&symbol='
     for data in datas:
         internal_id=data["FTID"]
         link=link_format+internal_id
