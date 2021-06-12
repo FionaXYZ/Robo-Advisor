3bf44e7273eae289e896a07f9adbab2d7fb31042
赵栩滢 <zhaoxuying@zhaoxuyingdeMacBook-Air.local>
2021-06-12 16:45:58 +0100

    new data

--- project/data/rate_sd.jl
+++ project/data/rate_sd.jl
@@ -1,10 +1,10 @@
-{"ISIN": "LU1053186349", "3_year_sd": "13.14 %"}
-{"ISIN": "IE00B3VNP587", "3_year_sd": "14.84 %"}
-{"ISIN": "LU1053186349", "3_year_annualised": "14.10"}
-{"ISIN": "IE00B530JS22", "3_year_annualised": "6.89"}
-{"ISIN": "IE00B3VNP587", "3_year_annualised": "10.41"}
-{"ISIN": "IE00B530JS22", "3_year_sd": "15.01 %"}
-{"ISIN": "LU1041109759", "3_year_annualised": "10.20"}
-{"ISIN": "LU1041109759", "3_year_sd": "14.63 %"}
-{"ISIN": "LU0712206050", "3_year_annualised": "0.74"}
-{"ISIN": "LU0712206050", "3_year_sd": "18.14 %"}
+{"ISIN": "LU1053186349", "3_year_annualised": "14.93"}
+{"ISIN": "LU0712206050", "3_year_annualised": "1.30"}
+{"ISIN": "IE00B530JS22", "3_year_sd": "16.18 %"}
+{"ISIN": "LU1041109759", "3_year_sd": "14.51 %"}
+{"ISIN": "LU0712206050", "3_year_sd": "17.87 %"}
+{"ISIN": "IE00B530JS22", "3_year_annualised": "7.32"}
+{"ISIN": "LU1053186349", "3_year_sd": "12.63 %"}
+{"ISIN": "LU1041109759", "3_year_annualised": "10.58"}
+{"ISIN": "IE00B3VNP587", "3_year_sd": "14.21 %"}
+{"ISIN": "IE00B3VNP587", "3_year_annualised": "12.29"}
